67fef1999adc06bacdce1b6835fc20a236b717ff
Shashank <65887227+pshashank64@users.noreply.github.com>
2021-07-20 17:26:37 +0530

    Update scrypt.js

--- scrypt.js
+++ scrypt.js
@@ -1,3 +1,37 @@
+$(document).scroll(function() {
+
+    myID1 = document.getElementById("candidate");
+
+    var myScroll = function () {
+        var y = window.scrollY;
+        if (y >= 350) {
+            myID1.className = "candidate af"
+        } else {
+            myID1.className = "candidate bef"
+        }
+    };
+
+    window.addEventListener("scroll", myScroll);
+});
+
+$(document).scroll(function() {
+
+    myID2 = document.getElementById("job-det-2");
+
+    var myScroll = function () {
+        var y = window.scrollY;
+        if (y >= 350) {
+            myID2.className = "job-det-2"
+        } else {
+            myID2.className = "job-det-2 bef"
+        }
+    };
+
+    window.addEventListener("scroll", myScroll);
+});
+
+
+
 
 $(document).scroll(function() {
 
