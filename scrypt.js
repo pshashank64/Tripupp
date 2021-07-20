d4bf6551ec841253d79e05e522bdd88a28a0e16b
Shashank <65887227+pshashank64@users.noreply.github.com>
2021-07-20 15:52:47 +0530

    Add files via upload

---
+++ scrypt.js
@@ -0,0 +1,16 @@
+
+$(document).scroll(function() {
+
+    myID = document.getElementById("anim-hh");
+
+    var myScrollFunc = function () {
+        var y = window.scrollY;
+        if (y >= 300) {
+            myID.className = "horizondal-menu show"
+        } else {
+            myID.className = "horizondal-menu hide"
+        }
+    };
+
+    window.addEventListener("scroll", myScrollFunc);
+});
